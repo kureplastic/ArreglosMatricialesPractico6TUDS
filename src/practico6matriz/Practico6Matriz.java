d62cec784317d6f0b1a243c807ebf7a16dd9e1c3
Fran Gonzalez <Fran Gonzalez@FRANGE>
2020-10-05 22:40:03 -0300

    Proyecto dde matrices

---
+++ src/practico6matriz/Practico6Matriz.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico6matriz;
+
+/**
+ *
+ * @author Fran Gonzalez
+ */
+public class Practico6Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Matriz mtrz = new Matriz();
+        
+        mtrz.armar();
+    }
+    
+}
